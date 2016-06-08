2909d2e891c997ae22c309d3ad87b7c574ed7ed8
Sean Rucker <sean@roverlabs.co>
2016-06-08 08:01:48 -0400

    Updates

---
+++ app.fdf062c79770ecd9ea21.js
@@ -0,0 +1,2 @@
+webpackJsonp([0,3],{0:function(n,e,o){o(1);var t=o(34);document.body.appendChild(t())},34:function(n,e){n.exports=function(){var n=document.createElement("h1");return n.className="pure-button",n.innerHTML="Hello world",n}}});
+//# sourceMappingURL=app.fdf062c79770ecd9ea21.js.map
